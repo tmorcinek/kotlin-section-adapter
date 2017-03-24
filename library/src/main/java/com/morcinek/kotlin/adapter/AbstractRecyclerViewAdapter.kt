9ecc505387b9f71096cdfc79fec5bcf42a849d56
Tomasz Morcinek <tomasz.morcinek@agoda.com>
2017-03-24 17:14:36 +0700

    Adding AbstractRecyclerViewAdapter.kt

---
+++ library/src/main/java/com/morcinek/kotlin/adapter/AbstractRecyclerViewAdapter.kt
@@ -0,0 +1,69 @@
+package com.morcinek.kotlin.adapter
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import java.util.*
+
+/**
+ * Copyright 2017 Tomasz Morcinek. All rights reserved.
+ */
+abstract class AbstractRecyclerViewAdapter<T, H : RecyclerView.ViewHolder>(protected var context: Context) : RecyclerView.Adapter<H>() {
+
+    protected val items = ArrayList<T>()
+
+    var itemClickListener: OnItemClickListener<T>? = null
+
+    inline fun setItemClickListener(crossinline function: (T) -> Unit) {
+        itemClickListener = object : OnItemClickListener<T> {
+            override fun onItemClicked(item: T, view: View) {
+                function.invoke(item)
+            }
+        }
+    }
+
+    fun setItemViewClickListener(function: (T, View) -> Unit) {
+        itemClickListener = object : OnItemClickListener<T> {
+            override fun onItemClicked(item: T, view: View) {
+                function.invoke(item, view)
+            }
+        }
+    }
+
+    protected fun getItem(position: Int): T {
+        return items[position]
+    }
+
+    fun setListAnimated(list: List<T>) {
+        items.clear()
+        notifyDataSetChanged()
+        items.addAll(list)
+        notifyItemRangeInserted(0, list.size)
+    }
+
+    fun clearListAnimated() {
+        notifyItemRangeRemoved(0, itemCount)
+        items.clear()
+    }
+
+    fun setList(list: List<T>) {
+        items.clear()
+        items.addAll(list)
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    protected fun initializeOnClickListener(item: T, view: View) {
+        view.setOnClickListener {
+            itemClickListener?.onItemClicked(item, it)
+        }
+    }
+
+    interface OnItemClickListener<T> {
+
+        fun onItemClicked(item: T, view: View)
+    }
+}
