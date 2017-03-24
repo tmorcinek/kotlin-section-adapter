daeb1b775b4bbb768a89ac998d9da4a13082e73a
Tomasz Morcinek <tomasz.morcinek@agoda.com>
2017-03-24 16:14:54 +0700

    Working SampleActivity.

---
+++ app/src/main/java/com/morcinek/kotlin/adapter/SampleActivity.kt
@@ -0,0 +1,13 @@
+package com.morcinek.kotlin.adapter
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+
+class SampleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+    }
+}
